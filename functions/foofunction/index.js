c68d3ed196f89855434497a1019b1065ac2034a3
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2021-10-01 14:33:23 -0400

    Modified function to create a distance calculation

--- functions/foofunction/index.js
+++ functions/foofunction/index.js
@@ -1,3 +1,7 @@
+'use strict';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const sampleData = require("./data/sample-data.json");    
 /**
  * Describe Foofunction here.
  *
@@ -12,11 +16,72 @@
  */
  
 export default async function (event, context, logger) {
-    logger.info(`Invoking Foofunction with payload ${JSON.stringify(event.data || {})}`);
+    
+    // logger.info(`Invoking Foofunction with payload ${JSON.stringify(event.data || {})}`);
+    // const results = await context.org.dataApi.query('SELECT Id, Name, AccountNumber FROM Account');
+    // logger.info(JSON.stringify(results));
+    // return results;
+    const data = event.data || {};
+    logger.info(
+      `Invoking processlargedatajs Function with payload ${JSON.stringify(data)}`
+    );
+  
+    // validate the payload params
+    if (!data.latitude || !data.longitude) {
+      throw new Error(`Please provide latitude and longitude`);
+    }
+  
+    // Sets 5 if length is not provided, also accepts length = 0
+    const length = data.length ?? 5;
+  
+    // Iterate through the schools in the file and calculate the distance using the distance function below
+    const schools = sampleData.schools
+      .map((school) => {
+        return Object.assign({}, school, {
+          distance: distance(
+            data.latitude,
+            data.longitude,
+            school.latitude,
+            school.longitude
+          )
+        });
+      })
+      // Sort schools by distance distance from the provided location
+      .sort((a, b) => parseFloat(a.distance) - parseFloat(b.distance));
+  
+    // Assign the nearest x schools to the results constant based on the length property provided in the payload
+    const results = schools.slice(0, length);
+  
+    // return the results
+    return { schools: results };
 
-    const results = await context.org.dataApi.query('SELECT Id, Name, AccountNumber FROM Account');
-
-    logger.info(JSON.stringify(results));
-
-    return results;
 }
+
+/**
+ * @param {string} latitudeSt:  represents the latitude of the origin point
+ * @param {string} longitudeSt:  represents the longitude of the origin point
+ * @param {string} latitudeSch:  represents the latitude of the school
+ * @param {string} longitudeSch:  represents the longitude of the school
+ * ....
+ * @returns {number} distance between point a and b
+ */
+ function distance(latitudeSt, longitudeSt, latitudeSch, longitudeSch) {
+    if (latitudeSt == latitudeSch && longitudeSt == longitudeSch) {
+      return 0;
+    } else {
+      const radLatitudeSf = (Math.PI * latitudeSt) / 180;
+      const radLatitudeSch = (Math.PI * latitudeSch) / 180;
+      const theta = longitudeSt - longitudeSch;
+      const radTheta = (Math.PI * theta) / 180;
+      let dist =
+        Math.sin(radLatitudeSf) * Math.sin(radLatitudeSch) +
+        Math.cos(radLatitudeSf) * Math.cos(radLatitudeSch) * Math.cos(radTheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = (dist * 180) / Math.PI;
+      dist = dist * 60 * 1.1515;
+      return dist;
+    }
+  }
